09bb0d1207fd2c6bfe80b76b9e4bf09db68c4b0a
Mayur Vaishnav <mayurvaishnav366@gmail.com>
2019-10-20 01:07:53 +0100

    Implemented Bernoulli and Binomial

--- server.R
+++ server.R
@@ -5,14 +5,52 @@ shinyServer(
 
     output$plot <- renderPlot({
       
+      switch (input$model,
+        'bernoulli' = {
+          par(mfrow=c(1,2))
+          Density <- density(rbinom(input$s,1,input$p))
+          plot(Density, main="Kernel Density of generated data")
+          polygon(Density, col="red", border="blue")
+          x=0:1
+          plot(x,dbinom(x,1,input$p))
+        },
+
+        'binomial' = {
+          par(mfrow=c(1,2))
+          d <- density(rbinom(input$s, input$n, input$p))
+          plot(d, main="Kernel Density of generated data")
+          polygon(d, col="red", border="blue")
+          x=0:input$n
+          plot(x,dbinom(x, input$n, input$p))
+        }
+      )
     })
 
-    output$summary <- renderText({
-      
+    output$summary <- renderPrint({
+
+      switch (input$model,
+        'bernoulli' = {
+          summary(rbinom(input$s, 1, input$p))
+        },
+
+        'binomial' = {
+          summary(rbinom(input$s, input$n, input$p))
+        }
+      )
     })
 
     output$table <- renderTable({
-      
+
+      switch (input$model,
+        'bernoulli' = {
+          c(rbinom(input$s,1,input$p))
+        },
+
+        'binomial' = {
+          c(rbinom(input$s, input$n,input$p))
+        }
+      )
+
     })
   }
 )
--- ui.R
+++ ui.R
@@ -1,26 +1,71 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Discrete Probability Models"),
+  headerPanel("Distrubition of Random Variables"),
   sidebarPanel(
-    selectInput("model", "Select Model",
+    radioButtons("modelType", "Type:",
       choices = c(
-        "Bernoulli" = "bernoulli",
-        "Binomial" = "binomial",
-        "Multinomial" = "multinomial",
-        "Poisson" = "poisson",
-        "Geometric" = "geometric",
-        "Hypergeometric" = "hypergeometric"
+        "Discrete" = "discrete",
+        "Continuous" = "continuous"
       ),
-      selected = "binomial"
+      selected = 'discrete',
+      inline = 'true'
+    ),
+
+    # Model Input according to Model Type
+    conditionalPanel(
+      condition = "input.modelType == 'discrete'",
+
+      selectInput("model", "Select Model",
+        choices = c(
+          "Bernoulli" = "bernoulli",
+          "Binomial" = "binomial",
+          "Multinomial" = "multinomial",
+          "Poisson" = "poisson",
+          "Geometric" = "geometric",
+          "Hypergeometric" = "hypergeometric"
+        ),
+        selected = "bernoulli"
+      ),
+
+      sliderInput("s", "Number of simulated data(s)", min=1, max=1000, value = 100),
+
+      conditionalPanel(
+        condition = "input.model == 'bernoulli'",
+        sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
+      ),
+
+      conditionalPanel(
+        condition = "input.model == 'binomial'",
+        numericInput("n", "Number of trails(n)", value = 10),
+        sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
+      )
+
+    ),
+
+    conditionalPanel(
+      condition = "input.modelType == 'continuous'",
+
+      selectInput("model", "Select Model",
+        choices = c(
+          "Uniform" = "uniform",
+          "Normal" = "normal",
+          "Exponential" = "exponential",
+          "Gamma" = "gamma",
+          "Chi-squared" = "chisquared"
+        ),
+        selected = "uniform"
+      )
+
     )
+
   ),
 
   mainPanel(
     tabsetPanel(type = "tabs",
-                tabPanel("Plot", plotOutput("plot")),
-                tabPanel("Summary", verbatimTextOutput("summary")),
-                tabPanel("Table", tableOutput("table"))
+      tabPanel("Plot", plotOutput("plot")),
+      tabPanel("Summary", verbatimTextOutput("summary")),
+      tabPanel("Table", tableOutput("table"))
     )
   )
 ))
