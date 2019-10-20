929a84d0287408b08212bcfc5a0849086951bf6c
Mayur Vaishnav <mayurvaishnav366@gmail.com>
2019-10-20 23:28:00 +0100

    Multinomial commented

--- server.R
+++ server.R
@@ -24,6 +24,15 @@ shinyServer(
           plot(x,dbinom(x, input$n, input$p))
         },
 
+#        'multinomial' = {
+#          par(mfrow=c(1,2))
+#          d <- table(rmultinom(input$s, input$n, input$p))
+#          barplot(d, col='red')
+#          x=0:input$n
+#          y=dmultinom(x, input$n, input$p)
+#          plot(x,y, type='b')
+#        },
+
         'poisson' = {
           par(mfrow=c(1,2))
           D=rpois(input$s, input$lam)
@@ -57,6 +66,10 @@ shinyServer(
           summary(rbinom(input$s, input$n, input$p))
         },
 
+#        'multinomial' = {
+#          summary(rmultinom(input$s, input$n, input$p))
+#        },
+
         'poisson' = {
           summary(rpois(input$s, input$lam))
         },
@@ -77,6 +90,10 @@ shinyServer(
         'binomial' = {
           c(rbinom(input$s, input$n, input$p))
         },
+
+#        'multinomial' = {
+#          c(rmultinom(input$s, input$n, input$p))
+#        },
 
         'poisson' = {
           c(rpois(input$s, input$lam))
--- ui.R
+++ ui.R
@@ -41,6 +41,12 @@ shinyUI(pageWithSidebar(
         sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
       ),
 
+#      conditionalPanel(
+#        condition = "input.model == 'multinomial'",
+#        numericInput("n", "Number of trails(n)", value = 10),
+#        sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
+#      ),
+
       conditionalPanel(
         condition = "input.model == 'poisson'",
         numericInput("max", "Upper limit for x" , value = 5),
