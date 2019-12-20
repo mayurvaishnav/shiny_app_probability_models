0f4236c21dbdc931d2b5721891e46cfc5f121d7e
Mayur Vaishnav <mayurvaishnav366@gmail.com>
2019-12-20 13:57:57 +0000

    Line comments for random variables

--- ui.R
+++ ui.R
@@ -1,12 +1,19 @@
+# Use the shiny library
 library(shiny)
+# Use the markdown library
 library(markdown)
 
-# shinyUI(pageWithSidebar(
+# Created the Tabs for each section like Random Variable, Prediction, Hypothesis test, GLM linear regression and About us
   navbarPage("Home",
+    # First Tab Random Variable
     tabPanel("Random  Variable",
-      headerPanel("Distrubition of Random Variables"),
+      # Header of this tab
+      headerPanel("Distribution of Random Variables"),
+      # Side bar for Random Variable panel
       sidebarLayout(
         sidebarPanel(
+          # Radio Button to select the type of probability models
+          # Default discrete is selected
           radioButtons("modelType", "Type:",
                        choices = c(
                          "Discrete" = "discrete",
@@ -16,10 +23,13 @@ library(markdown)
                        inline = 'true'
           ),
 
-          # Model Input according to Model Type
+          # Conditional panel for input for discrete probability models
           conditionalPanel(
+            # Checked the condition equal for input_moel type and discrete
             condition = "input.modelType == 'discrete'",
 
+            # List of all the Discrete models
+            # Default bernoulli is selected
             selectInput("model", "Select Model",
                         choices = c(
                           "Bernoulli" = "bernoulli",
@@ -31,43 +41,61 @@ library(markdown)
                         selected = "bernoulli"
             ),
 
+            # Slider input for simulated data
             sliderInput("s", "Number of simulated data(s)", min=1, max=1000, value = 10),
 
+            # Conditional panel for inputs of bernoulli
             conditionalPanel(
               condition = "input.model == 'bernoulli'",
+              # Slider input for the probability of successful trail
               sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
             ),
 
+            # Conditional panel for inputs of binomial
             conditionalPanel(
               condition = "input.model == 'binomial'",
+              # Numeric input for the number trail
               numericInput("n", "Number of trails(n)", value = 10),
+              # Slider input for the probability of successful trail
               sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
             ),
 
+            # Conditional panel for inputs of poission
             conditionalPanel(
               condition = "input.model == 'poisson'",
+              # Numeric input for the upper limit
               numericInput("max", "Upper limit for x" , value = 5),
+              # Numeric input for the lambda parameter
               numericInput("lam", "Parameter lambda in Poisson", value = 10)
             ),
 
+            # Conditional panel for inputs of geometric
             conditionalPanel(
               condition = "input.model == 'geometric'",
+              # Numeric input for the upper limit
               numericInput("max", "Upper limit for x" , value = 5),
+              # Slider input for the probability of successful trail
               sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
             ),
 
+            # Conditional panel for inputs of hypergeometric
             conditionalPanel(
               condition = "input.model == 'hypergeometric'",
+              # Numeric input for the M
               numericInput("m", "M" , value = 10),
+              # Numeric input for the N
               numericInput("n", "N" , value = 20),
+              # Numeric input for the K
               numericInput("k", "K" , value = 5)
             )
 
           ),
 
+          # Conditional panel for inputs of continuous probabilities models
           conditionalPanel(
             condition = "input.modelType == 'continuous'",
 
+            # Select input for all the Continuous models
             selectInput("modelCont", "Select Model",
                         choices = c(
                           "Uniform" = "uniform",
@@ -79,42 +107,62 @@ library(markdown)
                         selected = "uniform"
             ),
 
+            # Slider input for Number of simulated data
             sliderInput("s", "Number of simulated data" ,min=1, max=1000, value = 10),
+            # Numeric input for the support
             numericInput("i", "Support" , value = 2),
 
+            # Conditional panel for inputs of uniform
             conditionalPanel(
               condition = "input.modelCont == 'uniform'",
+              # Numeric input for the Parameter a
               numericInput("a", "Parameter a in Uniform" , value = -2),
+              # Numeric input for the Parameter b
               numericInput("b", "Parameter b in Uniform" , value = 0.8)
             ),
 
+            # Conditional panel for inputs of normal
             conditionalPanel(
               condition = "input.modelCont == 'normal'",
+              # Numeric input for the Parameter mu
               numericInput("mu", "Parameter mu in Normal" , value = 0),
+              # Numeric input for the Parameter sigma
               numericInput("sigma", "Parameter sigma in Normal" , value = 1)
             ),
 
+            # Conditional panel for inputs of exponential
             conditionalPanel(
               condition = "input.modelCont == 'exponential'",
+              # Numeric input for the Parameter lambda
               numericInput("lam", "Parameter lambda in exponential" , value = 1)
             ),
 
+            # Conditional panel for inputs of gamma
             conditionalPanel(
               condition = "input.modelCont == 'gamma'",
+              # Numeric input for the Parameter sigma
               numericInput("sigma", "Parameter sigma in Gamma" , value = 1),
+              # Numeric input for the Parameter lambda
               numericInput("lam", "Parameter lambda in Gamma" , value = 1)
             ),
 
+            # Conditional panel for inputs of chisquared
             conditionalPanel(
               condition = "input.modelCont == 'chisquared'",
-              numericInput("k", "Parameter K in Gamma" , value = 1)
+              # Numeric input for the Parameter K
+              numericInput("k", "Parameter K in chisquared" , value = 1)
             )
           )
         ),
+        # Main panel for Random Variable which shows the output
         mainPanel(
+          # Tabs in the main panel
           tabsetPanel(type = "tabs",
+                      # Plot tab which displays the graph
                       tabPanel("Plot", plotOutput("plot")),
+                      # Tab which displays the summary
                       tabPanel("Summary", verbatimTextOutput("summary")),
+                      # Tab which displays the table of simulated data
                       tabPanel("Table", tableOutput("table"))
           )
         )
